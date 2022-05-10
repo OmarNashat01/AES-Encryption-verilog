9f14e1f6ce4f321843f013390d8772fe671b2dc3
OmarNashat01 <omarnashat01@gmail.com>
2022-05-10 07:58:32 +0200

    Sboxall working.

--- AES.v
+++ AES.v
@@ -7,12 +7,16 @@ module AES(
 				input wire[191:0] Key192,
 				input wire[255:0] Key256,
 
-				output wire[127:0] out
+				output wire[127:0] out128,
+				output wire[127:0] out192,
+				output wire[127:0] out256
+				
 			);
 				
 
 				
-wire[127:0] data = Indata ^ Key128;
+wire[127:0] data0 = Indata ^ Key128;
+wire[127:0] data1;
 wire[127:0] data2;
 
 reg Sboxen = 1'b0;
@@ -24,33 +28,22 @@ reg Addrndkeyen = 1'b0;
 
 integer i;
 
+assign out128 = data2;
 
 
-Sboxall sbox(.sc(Sboxen), .Indata(data), .data(data2));
+Sboxall sbox0(.sc(clk), .Indata(data0), .data(data1));
 
-assign out = data2;
+Sboxall sbox1(.sc(clk), .Indata(data1), .data(data2));
 
-always @(clk)
-begin
-////	for (i = 0; i <  loops; i = i + 1)  
-////	begin
-////
-////
-////	end
-//
-//	
-		Sboxen = ~Sboxen;
-		
-//
-//
-//
-//
-end
+
+//integer loops = ( KeySize == 128 ) ? 9:
+//					 ( KeySize == 192 ) ? 11:
+//					 ( KeySize == 256 ) ? 13:
+//												  1;
+					 
+
 
 // Assign Output of your current step here to see it in tb
-
-
-
 
 
 
--- AES_tb.v
+++ AES_tb.v
@@ -8,29 +8,41 @@ reg[127:0] Key;
 wire[127:0] out;
 reg key;
 
-//AES encreptor(
-//					.KeySize(key),
-//					.clk(clk),
-//
-//					.Indata(Indata),
-//					.Key128(Key),
-//					.out(out)
-//					
-//					);
+AES encreptor(
+					.KeySize(key),
+					.clk(clk),
 
-Sboxall sbox(.sc(clk), .Indata(Indata), .data(out));									
+					.Indata(Indata),
+					.Key128(Key),
+					.out128(out)
+					
+					);
+									
 
 initial begin
 	
 	clk = 0;
 	
 	Indata = 	128'h3243f6a8885a308d313198a2e0370734;
-//	Key	 = 	128'h2b7e151628aed2a6abf7158809cf4f3c;
+	Key	 = 	128'h2b7e151628aed2a6abf7158809cf4f3c;
 	
-	#10
+	#20
 	
-	$display("Input = %h, out = %h",
-               Indata, out);
+	$display("Input = %h, Key = %h, XOR = %h, out = %h",
+               Indata, Key, Indata^Key, out);
+	
+//		
+//	#10
+//	
+//	$display("Input = %h, Key = %h, XOR = %h, out = %h",
+//               Indata, Key, Indata^Key, out);
+//					
+//					
+//						
+//	#10
+//	
+//	$display("Input = %h, Key = %h, XOR = %h, out = %h",
+//               Indata, Key, Indata^Key, out);
 	
 end
 
